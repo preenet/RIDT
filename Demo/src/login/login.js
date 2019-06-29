5316fe0f69cd57a9c2cd34cf82afbf43c1078ff6
JunYu Zhou <feijifeibudao@gmail.com>
2019-06-30 03:11:09 +0700

    build modules 2

--- Demo/src/login/login.js
+++ Demo/src/login/login.js
@@ -1,6 +1,8 @@
 import React from "react";
 import reactDom from "react-dom";
 import './login.css';
+//import Route from 'react-router-dom/Route'
+//import {BrowserRouter as Router} from 'react-router-dom'
 
 class Login extends React.Component {
     constructor(props) {
@@ -18,7 +20,11 @@ class Login extends React.Component {
 
     render() {
         return (
+
+            
+
             <div className="root-container">
+                <WelcomeBox />
                 <div className="box-controller">
                     <button type="button" className={"controller " + (this.state.isLoginOpen
                         ? "selected-controller"
@@ -41,7 +47,7 @@ class Login extends React.Component {
                     {this.state.isCommentOpen && <CommentBox />}
 
                 </div>
-                
+
             </div>
         );
 
@@ -49,18 +55,91 @@ class Login extends React.Component {
 
 }
 
-class LoginBox extends React.Component {
+class WelcomeBox extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
 
     }
 
-    submitLogin(e) {
+    render() {
+        return (
+            <div className="welcome">
+                Welcome to The Real-Tme Interactive Dashboard for Tourism!
+
+            </div>
+        );
+    }
+}
+
+class LoginBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { username: "", password: "", errors: [] };
 
     }
 
+
+    showValidationErr(elm, msg) {
+        this.setState((prevState) => ({ errors: [...prevState.errors, { elm, msg }] }));
+    }
+
+    clearValiadtionErr(elm) {
+        this.setState((prevState) => {
+            let newArr = [];
+            for (let err of prevState.errors) {
+                if (elm != err.elm) {
+                    newArr.push(err);
+                }
+            }
+            return { errors: newArr };
+        });
+    }
+
+    onUsernameChanged(e) {
+        this.setState({ username: e.target.value })
+        this.clearValiadtionErr("username");
+    }
+
+    onPasswordChanged(e) {
+        this.setState({ password: e.target.value })
+        this.clearValiadtionErr("password");
+    }
+
+    submitLogin(e) {
+        if (this.state.username == "") {
+            this.showValidationErr("username", "Username cannot be empty!")
+        }
+        if (this.state.password == "") {
+            this.showValidationErr("password", "Password cannot be empty!")
+        }
+        if (this.state.username != "" && this.state.password == "") {
+            this.setState({ username: e.target.value });
+            this.setState({ password: e.target.value });
+
+        }
+        console.log(this.state.username);
+        console.log(this.state.username);
+    }
+
+
+
+
+
     render() {
+
+        let usernameErr = null;
+        let passwordErr = null;
+
+        for (let err of this.state.errors) {
+            if (err.elm == "username") {
+                usernameErr = err.msg;
+            }
+            if (err.elm == "password") {
+                passwordErr = err.msg;
+            }
+        }
+
         return (
             <div className="inner-container">
 
@@ -72,14 +151,18 @@ class LoginBox extends React.Component {
 
                     <div className="input-group">
                         <label htmlFor="username">Username:</label><br />
-                        <input type="text" name="username" className="login-input" placeholder="Username" />
+                        <input type="text" name="username" className="login-input"
+                            value={this.state.username} onChange={this.onUsernameChanged.bind(this)}
+                            placeholder="Username" />
                     </div>
-
+                    <small className="danger-error">{usernameErr ? usernameErr : ""}</small>
                     <div className="input-group">
                         <label htmlFor="password">Password:</label><br />
-                        <input type="password" name="password" className="login-input" placeholder="Password" />
+                        <input type="password" name="password" className="login-input"
+                            value={this.state.password} onChange={this.onPasswordChanged.bind(this)}
+                            placeholder="Password" />
                     </div>
-
+                    <small className="danger-error">{passwordErr ? passwordErr : ""}</small>
                     <button type="button" className="login-btn" onClick={this.submitLogin.bind(this)} >Login</button>
 
                 </div>
@@ -93,15 +176,59 @@ class LoginBox extends React.Component {
 class CommentBox extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = { hotelName: "", review: "", errors: [] };
 
+    }
+
+    showValidationErr(elm, msg) {
+        this.setState((prevState) => ({ errors: [...prevState.errors, { elm, msg }] }));
+    }
+
+    clearValiadtionErr(elm) {
+        this.setState((prevState) => {
+            let newArr = [];
+            for (let err of prevState.errors) {
+                if (elm != err.elm) {
+                    newArr.push(err);
+                }
+            }
+            return { errors: newArr };
+        });
+    }
+
+    onHotelNameChanged(e) {
+        this.setState({ hotelName: e.target.value })
+        this.clearValiadtionErr("hotelName");
+    }
+
+    onReviewChanged(e) {
+        this.setState({ review: e.target.value })
+        this.clearValiadtionErr("review");
     }
 
     submitComment(e) {
-
+        if (this.state.hotelName == "") {
+            this.showValidationErr("hotelName", "Hotel name cannot be empty!")
+        }
+        if (this.state.review == "") {
+            this.showValidationErr("review", "Review cannot be empty!")
+        }
     }
 
     render() {
+
+        let hotelNameErr = null;
+        let reviewErr = null;
+
+        for (let err of this.state.errors) {
+            if (err.elm == "hotelName") {
+                hotelNameErr = err.msg;
+            }
+            if (err.elm == "review") {
+                reviewErr = err.msg;
+            }
+        }
+
         return (
             <div className="inner-container">
 
@@ -113,13 +240,18 @@ class CommentBox extends React.Component {
 
                     <div className="input-group">
                         <label htmlFor="hotelname">Hotel Name:</label><br />
-                        <input type="text" name="hotelname" className="comment-input" placeholder="Hotel Name" />
+                        <input type="text" name="hotelname" className="comment-input"
+                            onChange={this.onHotelNameChanged.bind(this)} placeholder="Hotel Name" />
                     </div>
+
+                    <small className="danger-error">{hotelNameErr ? hotelNameErr : ""}</small>
 
                     <div className="input-group">
                         <label htmlFor="review">Review:</label><br />
-                        <input type="text" name="review" className="comment-input" placeholder="Review" />
+                        <input type="text" name="review" className="comment-input" onChange={this.onReviewChanged.bind(this)} placeholder="Review" />
                     </div>
+
+                    <small className="danger-error">{reviewErr ? reviewErr : ""}</small>
 
                     <button type="button" className="comment-btn" onClick={this.submitComment.bind(this)} >Comment</button>
 
