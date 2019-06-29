df532c2be977d4ab71c893832d1956e23e1ac335
JunYu Zhou <feijifeibudao@gmail.com>
2019-06-29 23:22:14 +0700

    feature 1 UI

--- Demo/src/login/login.js
+++ Demo/src/login/login.js
@@ -4,24 +4,35 @@ import reactDom from "react-dom";
 class Login extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { };
+        this.state = { isLoginOpen: true, isCommentOpen: false };
     }
 
-    
-    
+    showLoginBox(){
+        this.setState({isLoginOpen: true, isCommentOpen: false});
+    }
+
+    showCommentBox(){
+        this.setState({isLoginOpen: false, isCommentOpen: true});
+    }
+
     render() {
         return (
             <div className="root-container">
                 <div className="box-controller">
-                    <div className="controller">
+                    <button type="button" className="login-page" onClick={this.showLoginBox.bind(this)}>
                         Login Page
-                    </div>
+                    </button>
+
+                    <button type="button" className="comment-page" onClick={this.showCommentBox.bind(this)}>
+                        Comment Page
+                    </button>
+
                 </div>
 
                 <div className="box-container">
 
-                    <LoginBox />
-
+                    {this.state.isLoginOpen && <LoginBox    />}
+                    {this.state.isCommentOpen && <CommentBox    />}
                 </div>
 
             </div>
@@ -43,30 +54,71 @@ class LoginBox extends React.Component {
     }
 
     render() {
-        return(
-        <div className="inner-container">
+        return (
+            <div className="inner-container">
 
-            <div className="header">
-                Please Login
+                <div className="header">
+                    Please Login
             </div>
 
-            <div className="box">
+                <div className="box">
 
-                <div className="input-group">
-                    <label htmlFor="username">Username</label><br/>
-                    <input type="text" name="username" className="login-input" placeholder="Username" />
+                    <div className="input-group">
+                        <label htmlFor="username">Username:</label><br />
+                        <input type="text" name="username" className="login-input" placeholder="Username" />
+                    </div>
+
+                    <div className="input-group">
+                        <label htmlFor="password">Password:</label><br />
+                        <input type="password" name="password" className="login-input" placeholder="Password" />
+                    </div>
+
+                    <button type="button" className="login-btn" onClick={this.submitLogin.bind(this)} >Login</button>
+
                 </div>
-
-                <div className="input-group">
-                    <label htmlFor="password">Password  </label><br/>
-                    <input type="password" name="password" className="login-input" placeholder="Password" />
-                </div>
-
-                <button type="button" className="login=btn" onClick={this.submitLogin.bind(this)} >Login</button>
 
             </div>
+        );
+    }
 
-        </div>
+}
+
+class CommentBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+
+    }
+
+    submitComment(e) {
+
+    }
+
+    render() {
+        return (
+            <div className="inner-container">
+
+                <div className="header">
+                    Please Input Your Comment
+            </div>
+
+                <div className="box">
+
+                    <div className="input-group">
+                        <label htmlFor="hotelname">Hotel Name:</label><br />
+                        <input type="text" name="hotelname" className="comment-input" placeholder="Hotel Name" />
+                    </div>
+
+                    <div className="input-group">
+                        <label htmlFor="review">Review:</label><br />
+                        <input type="text" name="review" className="comment-input" placeholder="Review" />
+                    </div>
+
+                    <button type="button" className="comment-btn" onClick={this.submitComment.bind(this)} >Comment</button>
+
+                </div>
+
+            </div>
         );
     }
 
